6293ee4c3c599510a37e69b2e23134b3b5dc0bee
MekaKoatl <carlos-raf@outlook.com>
2026-05-29 18:58:09 +0200

    Sucess conexion back y front con solo Mongo

--- api/registroUsuario.js
+++ api/registroUsuario.js
@@ -1,1 +1,16 @@
 //
+function pruebausers() {
+  fetch("https://backendredsocial.vercel.app/usuarios/seeusers")
+    .then((res) => res.json())
+    .then((data) => {
+      data.forEach((usuario) => {
+        document.getElementById("users").innerHTML += `
+          <p>${usuario.username}</p>
+        `;
+      });
+    });
+}
+
+pruebausers();
+
+
