fde795ae03f4de86f9f76d21433e9eb867e4d5a9
Luca0Vega <vega2999bueno@alumnostrazos.net>
2026-05-29 17:07:35 +0200

    formularioregistroylogininiciado

--- registro/registro.js
+++ registro/registro.js
@@ -1,1 +1,23 @@
-//
+function register(usuario){
+    fetch(`http://localhost:3000/api/usuarios/registro`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({usuario}),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        const username = document.getElementById("user").value
+        const email = document.getElementById("email").value
+        const password = document.getElementById("password").value
+
+        for(let i = 0; i < data.usuarios.length; i++){
+            if(password.length < 6){document.getElementById("userAlert").style.color = "red" }
+            if(data.res.status(400)){document.getElementById("userAlert").innerText = data.res.message}
+            if(f){f}
+        }
+
+
+        });
+      }
