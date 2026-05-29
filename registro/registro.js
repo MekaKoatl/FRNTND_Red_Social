0bd3343ceaa3ef1ce85d17b7beee0669b8a17b5c
Luca0Vega <vega2999bueno@alumnostrazos.net>
2026-05-29 18:14:10 +0200

    prueba fetch

--- registro/registro.js
+++ registro/registro.js
@@ -1,23 +1,34 @@
-function register(usuario){
-    fetch(`http://localhost:3000/api/usuarios/registro`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({usuario}),
-    })
-      .then((res) => res.json())
-      .then((data) => {
-        const username = document.getElementById("user").value
-        const email = document.getElementById("email").value
-        const password = document.getElementById("password").value
+// function register(usuario){
+//     fetch(`http://localhost:3000/api/usuarios/registro`, {
+//       method: "POST",
+//       headers: {
+//         "Content-Type": "application/json",
+//       },
+//       body: JSON.stringify({usuario}),
+//     })
+//       .then((res) => res.json())
+//       .then((data) => {
+//         const username = document.getElementById("user").value
+//         const email = document.getElementById("email").value
+//         const password = document.getElementById("password").value
 
-        for(let i = 0; i < data.usuarios.length; i++){
-            if(password.length < 6){document.getElementById("userAlert").style.color = "red" }
-            if(data.res.status(400)){document.getElementById("userAlert").innerText = data.res.message}
-            if(f){f}
-        }
+//         for(let i = 0; i < data.usuarios.length; i++){
+//             if(password.length < 6){document.getElementById("userAlert").style.color = "red" }
+//             //if(data.res.status(400)){document.getElementById("userAlert").innerText = data.res.message}
+//             if(){}
+//         }
+
+//         });
+//       }
+
+  fetch("http://localhost:3000/api/usuarios/registro")
+    .then((res) => res.json())
+    .then((data) => {
+      data.data.forEach((getusers) => {
+        document.getElementById("users").innerHTML += `
+            <p>${data.username}</p>
+           `;
+      });
+    });
 
 
-        });
-      }
