7669d9eec28087a959a2a89b13950d452bc8aa45
Hugo <hugomdmg@profesorestrazos.net>
2026-06-02 18:36:21 +0200

    buscador content div y boton de entrar en buscador

--- index.js
+++ index.js
@@ -5,3 +5,12 @@ function changePage(page) {
       document.getElementById("content").innerHTML = html;
     });
 }
+
+
+function getBuscador() {
+  fetch("./app/buscador.html")
+    .then((res) => res.text())
+    .then((html) => {
+      document.getElementById("buscador-content").innerHTML = html;
+    });
+}
