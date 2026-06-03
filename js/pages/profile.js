31349137abd8a704f1c6989868451a11e2084f50
MekaKoatl <carlos-raf@outlook.com>
2026-06-03 18:55:25 +0200

    correxion

--- js/pages/profile.js
+++ js/pages/profile.js
@@ -13,37 +13,37 @@ export function cargarProfile() {
 
 export function cargarFeedProfile() {
 
-  const userId = localStorage.getItem('userId');
-    const content = document.getElementById('user-posts');
+  // const userId = localStorage.getItem('userId');
+  //   const content = document.getElementById('user-posts');
   
   
-    // Obtener posts de seguidos
-    const posts = await getUserPosts(userId);
+  //   // Obtener posts de seguidos
+  //   const posts = await getUserPosts(userId);
   
-    if (posts.length === 0) {
-      content.innerHTML = '<p>No hay posts aún.</p>';
-      return;
-    }
+  //   if (posts.length === 0) {
+  //     content.innerHTML = '<p>No hay posts aún.</p>';
+  //     return;
+  //   }
   
-    // Renderizar posts
-    content.innerHTML = posts.map(post => `
-      <article class="post-card">
-        <p class="post-username">${post.username}</p>
-        <div class="post-container">
-          <div class="post-body">
-            <p>${post.body}</p>
-          </div>
-          <div class="post-actions">
-            <div class="post-actions-left">
-              <button class="action-btn"></button>
-              <button class="action-btn"></button>
-              <button class="action-btn"></button>
-            </div>
-            <div class="post-actions-right">
-              <button class="action-btn"></button>
-            </div>
-          </div>
-        </div>
-      </article>
-    `).join('');
+  //   // Renderizar posts
+  //   content.innerHTML = posts.map(post => `
+  //     <article class="post-card">
+  //       <p class="post-username">${post.username}</p>
+  //       <div class="post-container">
+  //         <div class="post-body">
+  //           <p>${post.body}</p>
+  //         </div>
+  //         <div class="post-actions">
+  //           <div class="post-actions-left">
+  //             <button class="action-btn"></button>
+  //             <button class="action-btn"></button>
+  //             <button class="action-btn"></button>
+  //           </div>
+  //           <div class="post-actions-right">
+  //             <button class="action-btn"></button>
+  //           </div>
+  //         </div>
+  //       </div>
+  //     </article>
+  //   `).join('');
 }
