5e33ec17826911d4bd5e0398f92b7c1421b85086
Luca0Vega <vega2999bueno@alumnostrazos.net>
2026-06-04 11:40:44 +0200

    update buscador a ciegas igualmente :,)

--- js/pages/buscador.js
+++ js/pages/buscador.js
@@ -1,9 +1,15 @@
+//import { getusers } from '../api/usuarios.js';
+
+import { BASE_URL } from './config.js';
+
+//`${BASE_URL}/api/usuarios/seeusers`
+
 function buscar() {
   let searchValue = document.getElementById("buscarInput").value;
   console.log(searchValue);
-  fetch("https://backendredsocial.vercel.app//usuarios/seeusers")
+  fetch(`${BASE_URL}/api/usuarios/seeusers`)
     .then(function (response) {
-      return response.json();
+      return res.json(getusers);
     })
     .then(function (data) {
       document.getElementById("usuariosBuscados").innerHTML = "";
@@ -11,17 +17,15 @@ function buscar() {
         if (
           data[i].usuario.username //o solo username sin usuario
             .toLowerCase()
-            .substring(i - 1, data[i].usuario.username.length)
+            .substring(i - 1, data[i].username.length)//cambiar a searchvalue.lenght? si no funciona con data user
             .includes(searchValue.toLowerCase())
         ) {
           document.getElementById("usuariosBuscados").innerHTML +=
-            `<div class="movie"><h2>${data[i].usuario}</h2>
-                                                      <img src=${data[i].image_url}>
-                                                      <div class="movfilterinfo">
-                                                      <p class="username">${data[i].usuario.username}</p>
-                                                      <button onclick="verPerfil()"></button>
+                                                      `<div class="usersearched">
+                                                      <p class="username">${data[i].username}</p>
+                                                      <button onclick="s">ver perfil</button>
                                                       </div>`;
-          document.getElementById("mov").style.display = "flex";
+          document.getElementById("usuariosBuscados").style.display = "flex";
         }
       }
     });
