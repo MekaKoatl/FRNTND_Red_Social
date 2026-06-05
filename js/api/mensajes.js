ab99697d61113ca791a0b779620771e77b61139c
Luca0Vega <vega2999bueno@alumnostrazos.net>
2026-06-05 18:50:44 +0200

    corregir funcion

--- js/api/mensajes.js
+++ js/api/mensajes.js
@@ -18,9 +18,28 @@ export async function chat(contactId){
     content.innerHTML = html;
     console.log(contactId, userId)
     if(contactId){document.getElementById("userContact").innerText = contactId
-}}
+    fetch(`${BASE_URL}/mensajes/${userId}`,)
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data)
+    });
+    }}
 
 
 export async function getMessage(){
+  fetch(`${BASE_URL}/mensajes`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ nombre: name, apellido: surname, dni: dni }),
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data)
+document.getElementbyId("mensajesChat").innerHTML = `<div id="mensajeUser"><p>mensaje el user</p></div>
 
+<div id="mensajePropio"><p>mensaje mio</p></div>`
+})
 }
+
